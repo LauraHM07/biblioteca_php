3a930c601827d5e835c93b6c8650ddd3b53bde28
Laura Haro <laura.haro.962@estudiante.iesoretania.es>
2022-10-05 12:37:09 +0200

    Eliminar libro

---
+++ php/eliminarLibro.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Borrar libro</title>
+</head>
+<body>
+    <h1>BORRAR LIBRO</h1>
+
+    <?php
+    
+    ?>
+</body>
+</html>
